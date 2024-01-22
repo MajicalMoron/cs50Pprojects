c2c3507f44f37d4815a124a56169d4f83c492acc
MajicalMoron <153124948+MajicalMoron@users.noreply.github.com>
2024-01-22 07:23:33 -0500

    1/22/24 morning update

--- lecture1/lecture_1_proof.py
+++ lecture1/lecture_1_proof.py
@@ -4,11 +4,13 @@ x = int(input("What's x? "))
 y = int(input("What's y? "))
 fn = input("What opperation: ")
 
-if fn == "Multiplication" or fn == "Times":
-    print(x * y)
-elif fn == "Subtraction" or fn == "Minus":
-    print(x - y)
-elif fn == "Division" or fn == "Divide":
-    print(x / y)
-elif fn == "Addition" or fn == "Add":
-    print(x + y)
+def opperation(fn):
+    if fn == "Multiplication" or fn == "Times":
+        print(x * y)
+    elif fn == "Subtraction" or fn == "Minus":
+        print(x - y)
+    elif fn == "Division" or fn == "Divide":
+        print(x / y)
+    elif fn == "Addition" or fn == "Add":
+        print(x + y)
+opperation()
