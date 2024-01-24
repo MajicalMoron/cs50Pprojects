d0649ab6dc5aa7aab823ad60161d3dc9fbb3c550
MajicalMoron <153124948+MajicalMoron@users.noreply.github.com>
2024-01-24 10:27:45 -0500

    1-24-24 end of class update

--- lecture2/hogwarts.py
+++ lecture2/hogwarts.py
@@ -1,3 +1,34 @@
-students = ["Hermione", "Harry", "Ron"]
+#a list of dictonarys
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patrounus": "otter"},
+    {"name": "Harry", "house": "Gryffindor", "patrounus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patrounus": "Jack Rustell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patrounus": None}
+]
 
-print(students)
+for student in students:
+    print(student["name"], student["house"], student["patrounus"], sep=", ")
+
+'''
+students = {
+    "Hermione": "Gryffindor", 
+    "Harry": "Gryffindor", 
+    "Ron": "Gryffindor", 
+    "Draco": "Slytherin"
+}
+
+for student in students:
+    print(student, students[student], sep = ", ")
+
+    
+#uses student's name acts as the indicator of location 
+print(students["Hermione"])
+
+#creates the var "students" and makes is a list 
+students = ["Hermione", "Harry", "Ron", "Draco"]
+houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+
+# len means length, uses var "students" to tell how long a list is 
+for i in range(len(students)):
+    print(i + 1, students[i])
+    '''
---
+++ lecture2/mario.py
@@ -0,0 +1,5 @@
+def main():
+    print_colum(3)
+
+def print_colum(height):
+    print("#\n" * height, end="")
