19125b7f5dcf632260577ca7432c3ea0ebfe73e6
MajicalMoron <153124948+MajicalMoron@users.noreply.github.com>
2024-01-26 10:21:54 -0500

    end of class

---
+++ proof.py
@@ -0,0 +1,24 @@
+#show proof that I did stuff 
+# gonna be a calculator 
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+fn = input("What opperation: ")
+
+opperation = [
+    {"name", "Multiplication", "Times", "Multiply" }
+]
+
+if fn == opperation'''
+
+'''
+def equation(fn):
+    if fn == "Multiplication" or fn == "Times":
+        print(x * y)
+    elif fn == "Subtraction" or fn == "Minus":
+        print(x - y)
+    elif fn == "Division" or fn == "Divide":
+        print(x / y)
+    elif fn == "Addition" or fn == "Add":
+        print(x + y)
+'''
+equation()
