57ed83be7c019b4b64107c56e39295d78cede3c4
MajicalMoron <153124948+MajicalMoron@users.noreply.github.com>
2024-02-01 10:21:32 -0500

    end of class 2-1-24

--- proof.py
+++ proof.py
@@ -4,28 +4,42 @@ x = int(input("What's x? "))
 y = int(input("What's y? "))
 fn = input("What opperation: ").title
 z = 0
-
+'''
 opperation = [
     {"name": "Multiplication" or "Times" or "Multiply"},
     {"name": "Divide" or "Division" or "Over"},
     {"name": "Subtract" or "Subtraction" or "Minus" or "Less"},
     {"name": "Add" or "Addition" or "Plus"},
-]
+]'''
+
+# making the array of lists, for each type of (basic) math problem
+opperation = {
+    "mult": "Multiplication" or "Times" or "Multiply",
+    "divi": "Divide" or "Division" or "Over",
+    "sub": "Subtract" or "Subtraction" or "Minus" or "Less",
+    "add": "Add" or "Addition" or "Plus",
+}
 
 # Opperation Transvers
-def opp_tr():
-    p = str("place holder")
-    p1 = 0
-    #i = 0
-    for i in range(opperation("name"[p1])):
-        p = opperation["name"[i]]
-        if fn == p:
-            return(p)
-        if i < range(opperation(p1)):
-            p1 = p1 + 1
-            i = 0
-    
+def opp_tr(p): 
+    for i in range(opperation[p]):
+        r = opperation[p, i]
+        if fn == r:
+            return(r)
 
+# checks if the users input is a usable argument
+if fn == opperation["multi"]:
+    opp_tr("multi")
+elif fn == opperation["divi"]:
+    opp_tr("divi")
+elif fn == opperation["sub"]:
+    opp_tr("sub")
+elif fn == opperation["add"]:
+    opp_tr("add")
+else:
+    print("Your a falure of a person. \n" + "Have a great day!")
+
+# does the actual math problem
 def equation():
     if fn == opperation["name"]:
         z =  x * y
@@ -40,7 +54,7 @@ def equation():
         z = x + y
         z = z + " -Addition"
     else:
-        z = "Your a falure."
+        z = "Your a falure.\n" + "Thanks for trying tho"
     return(z)
     
 
@@ -56,8 +70,6 @@ def equation(fn):
         print(x + y)
 '''
 
+opp_tr()
+print(equation(fn))
 
-
-print(opp_tr())
-equation(fn)
-print(z)
